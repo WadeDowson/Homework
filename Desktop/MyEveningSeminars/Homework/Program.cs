1d75024d9a32790e81fea9d84232c1880bdeb3c9
Wade <frolover683@gmail.com>
2022-12-27 20:36:07 +0300

    Домашняя работа

---
+++ Desktop/MyEveningSeminars/Homework/Program.cs
@@ -0,0 +1,67 @@
+﻿
+// // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// // a = 5; b = 7 -> max = 7
+// // a = 2 b = 10 -> max = 10
+// // a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (b>max) max=b;
+// Console.WriteLine($"max = {max}");
+
+// // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// // 2, 3, 7 -> 7
+// // 44 5 78 -> 78
+// // 22 3 9 -> 22
+
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (b>max) max=b;
+// if (c>max) max=c;
+//  Console.WriteLine($"max = {max}");
+
+// // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// // 4 -> да
+// // -3 -> нет
+// // 7 -> нет
+
+// Console.WriteLine("Input number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int residue = a % 2;
+// if (residue==0)
+// {
+//     Console.WriteLine($"{a} -  четное число");
+// }
+// else
+// {
+//     Console.WriteLine($"{a} -  нечетное число");
+// }
+
+// // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// // 5 -> 2, 4
+// // 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Input number");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int a = 2;
+// while (a<N)
+// {
+//     Console.Write($"{a}, ");
+//     a=a+2;
+// }
+
+
+
+
+
+
+
+
